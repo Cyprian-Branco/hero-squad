f553a06489711c581661086f47ca5401a496a2ec
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-17 21:14:04 +0300

    initialize all the variables

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    public static ArrayList<Hero> instances = new ArrayList<>();
+    private int id;
+    private int squadId;
+
+    public Hero(String name,int age, String specialPower, String weakness, int squadId){
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        this.squadId = squadId;
+    }
 }
