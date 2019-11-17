075e2c74568460e984b1fb7717eeb0a0aff64a2f
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-17 16:46:17 +0300

    add getter methods

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,17 +1,24 @@
 public class Squad {
     private String name;
     private String cause;
-    private int size;
+    private int max_size;
 
-    public Squad(String name, String cause, int size){
+    public Squad(String name, String cause, int max_size){
         this.name = name;
         this.cause = cause;
-        this.size = size;
+        this.max_size = max_size;
     }
 
     public String getName() {
         return name;
     }
 
-    
+    public String getCause() {
+        return cause;
+    }
+
+    public int getMax_size() {
+        return max_size;
+    }
+
 }
