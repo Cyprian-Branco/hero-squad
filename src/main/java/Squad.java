c1fefb9f9aa735772d2d5e31c635b135e27f0420
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-17 14:57:27 +0300

    declare encapsulated variables

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,11 @@
-public class squad {
+public class Squad {
+    private String name;
+    private String cause;
+    private int size;
+
+    public Squad(String name, String cause, int size){
+        this.name = name;
+        this.cause = cause;
+        this.size = size;
+    }
 }
