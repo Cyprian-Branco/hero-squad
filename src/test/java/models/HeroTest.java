5665f5d814576856d4d0e1947a7dc8e0f0e7e92a
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-17 21:12:15 +0300

    test input variables of the hero

---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,22 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    @Test
+    public void testHeroInstantiatesCorrectly_true(){
+        Hero hero = new Hero("WonderWoman", 64, "Throwing Fire", 2)
+    }
+}
