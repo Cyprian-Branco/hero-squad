a3ea7b7b19c097fde1a7d59404691cd43517b0e2
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-17 19:22:49 +0300

    fetch all form data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,12 @@
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
+
 
 public class App {
     public static void main(String[] args) {
@@ -14,6 +15,18 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squad/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/squad/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String cause = request.queryParams("cause");
+            int max_size = Integer.parseInt(request.queryParams("max_size"));
+            Squad squad =  new Squad(name, cause, max_size);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
