7a23eb53968b3213f1ad003eb4ee0da9d5a75614
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-18 14:12:04 +0300

    add getter methods

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Hero {
     private String name;
@@ -17,5 +18,39 @@ public class Hero {
         this.specialPower = specialPower;
         this.weakness = weakness;
         this.squadId = squadId;
+        instances.add(this);
+        this.id = instances.size();
+        addHeroToSquad(squadId);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public int getSquadId() {
+        return squadId;
+    }
+    public static ArrayList<Hero> getInstances() {
+        return instances;
+    }
+    public void addHeroToSquad(int squadID){
+        for(Squad squad:Squad.getInstances()){
+            if(squad.getId() == squadID){
+                squad.addMember(this);
+                break;
+            }
+        }
     }
 }
